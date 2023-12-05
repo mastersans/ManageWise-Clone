0174850340c830ea17c57f5189cd6a4d3d080d17
Sanskar Sharma <sanskarsharma3110@gmail.com>
2023-12-05 22:52:48 +0530

    fix: Overflow and Scrollbar

--- src/Components/TestimonialSlider/TestimonialSlider.js
+++ src/Components/TestimonialSlider/TestimonialSlider.js
@@ -6,51 +6,70 @@ import './TestimonialSlider.css';
 const testimonialsData = [
   {
     id: 1,
+    quote: "Manage Wise transformed our team's productivity. The intuitive interface and powerful features have made a significant impact on our daily operations",
+    photoUrl: "/Emily.webp",
+    name: "Emily",
+    designation: "Art Director",
+  },
+  {
+    id: 2,
     quote: "The customer support team behind this product is exceptional. They have been incredibly responsive and helpful, addressing any questions or concerns promptly.",
     photoUrl: "/Sarah.webp",
     name: "Sarah",
     designation: "Project Manager",
   },
   {
-    id: 2,
+    id: 3,
     quote: "Using this product has been a game-changer for our team. The collaboration features and intuitive interface have greatly improved our productivity.",
     photoUrl: "/David.webp",
     name: "David",
     designation: "Freelancer",
   },
   {
-    id: 3,
+    id: 4,
     quote: "I can't imagine running my business without this product. The automation features have saved me hours of manual work, allowing me to focus on strategic tasks",
     photoUrl: "/Alex.webp",
     name: "Alex",
     designation: "IT Manager",
   },
   {
-    id: 4,
+    id: 5,
     quote: "Manage Wise transformed our team's productivity. The intuitive interface and powerful features have made a significant impact on our daily operations",
     photoUrl: "/Emily.webp",
     name: "Emily",
     designation: "Art Director",
   },
+  {
+    id: 6,
+    quote: "The customer support team behind this product is exceptional. They have been incredibly responsive and helpful, addressing any questions or concerns promptly.",
+    photoUrl: "/Sarah.webp",
+    name: "Sarah",
+    designation: "Project Manager",
+  },
+  {
+    id: 7,
+    quote: "Using this product has been a game-changer for our team. The collaboration features and intuitive interface have greatly improved our productivity.",
+    photoUrl: "/David.webp",
+    name: "David",
+    designation: "Freelancer",
+  },
+  {
+    id: 8,
+    quote: "I can't imagine running my business without this product. The automation features have saved me hours of manual work, allowing me to focus on strategic tasks",
+    photoUrl: "/Alex.webp",
+    name: "Alex",
+    designation: "IT Manager",
+  },
 ];
 
 const TestimonialsSlider = () => {
-/*  const sliderRef = useRef(null);
-
-  useEffect(() => {
-    const slider = sliderRef.current;
-    const testimonials = slider.querySelectorAll('.client-testimonial');
-    const totalWidth = testimonials.length * testimonials[0].offsetWidth;
-
-    slider.style.width = `${totalWidth}px`;
-  }, []);*/
 
   return (
     <div name="Testimonial">
     <div className="testimonial-st"><SectionTitle emoji="🧡" text="Testimonial"/></div>
     <div className="testimonial-title">Hear from our&nbsp;<span className="highlighted"> Satisfied </span>&nbsp;clients</div>
     <div className="testimonial-description">Discover why our clients love working with us. Read their<br/> testimonials and learn how we has helped businesses.</div>
-    <div className="client-testimonials-slider animate"/* ref={sliderRef}*/>
+    <div className="client-testimonials-slider animate">
       {testimonialsData.map((testimonial) => (
         <Testimonial
           key={testimonial.id}
