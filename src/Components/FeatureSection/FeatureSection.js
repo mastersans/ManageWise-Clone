80ce2dd93800ea5ee4a4e6484a04c20fccf032f5
Sanskar Sharma <sanskarsharma3110@gmail.com>
2023-12-05 17:19:44 +0530

    fix: ScrollAnimation

--- src/Components/FeatureSection/FeatureSection.js
+++ src/Components/FeatureSection/FeatureSection.js
@@ -1,11 +1,12 @@
 import React, {useEffect} from "react";
 import "./FeatureSection.css";
 import Aos from "aos";
+import "aos/dist/aos.css"
 import SectionTitle from "../SectionTitle/SectionTitle";
 import AllFeatures from "../AllFeatures/AllFeatures";
 const FeatureSection = () => {
     useEffect(() => {
-        Aos.init();
+        Aos.init({ duration: 2000 });
     }, []);
     return (
         <div className="feature-sec">
