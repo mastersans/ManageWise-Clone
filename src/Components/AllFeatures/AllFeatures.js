e412340676f153a436f27a4231d9f105a010edc8
Sanskar Sharma <sanskarsharma3110@gmail.com>
2023-12-05 17:22:40 +0530

    fix: ScrollAnimation

--- src/Components/AllFeatures/AllFeatures.js
+++ src/Components/AllFeatures/AllFeatures.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Aos from "aos";
 import './AllFeatures.css';
+import "aos/dist/aos.css"
 import Feature from '../Features/Feature'
 import BigTile from '../BigTile/BigTile'
 import NonImageFeature from '../Features/NonImageFeature'
 const AllFeatures = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 });
+    }, []);
     return (
     <div className="all-features">
         <div className="upper-features">
